c09f370f060cf38e68d9c2c6e97bbff863939e24
Jesus <barthjoss@hotmail.es>
2022-07-16 22:31:02 -0500

    Advances

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,7 @@
 
     <!-- Styles -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link href="{{ asset('public/css/styles_biblio.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/styles_biblio.css') }}" rel="stylesheet">
     {{-- <link href="{{ asset('resources/app.css') }}" rel="stylesheet"> --}}
 </head>
 
